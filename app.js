bd4ce2aae92fb19593efd173f49d847aead48ecb
LalitDhane <lalitdhane4455@gmail.com>
2021-06-04 17:12:52 +0530

    create dynamic list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}))
+let tasks = []
 app.get('/',(req,res)=>{
     
      const date = new Date()
@@ -10,7 +12,12 @@ app.get('/',(req,res)=>{
       day:'numeric' 
     }
     let headingdate = date.toLocaleDateString('en-us',options)
-    res.render('list',{headingdate:headingdate})
+    res.render('list',{headingdate:headingdate,todolist:tasks})
+})
+app.post('/',(req,res)=>{
+  let task = req.body.task; 
+  tasks.push(task)   
+  res.redirect('/')
 })
 app.listen(3000,()=>{
   console.log('server is running at port 3000')
