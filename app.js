2a5cc9f96f021cb16d4cf2e8cb014667872c08dd
LalitDhane <lalitdhane4455@gmail.com>
2021-06-05 09:41:09 +0530

    link css files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
 let tasks = []
 app.get('/',(req,res)=>{
     
