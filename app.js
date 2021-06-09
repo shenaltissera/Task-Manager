0b03c6f6061834600870960c57fd14c314bb2f2e
LalitDhane <lalitdhane4455@gmail.com>
2021-06-09 16:59:25 +0530

    add work route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 let tasks = []
+let worktasks = []
 app.get('/',(req,res)=>{
     
      const date = new Date()
@@ -13,12 +14,21 @@ app.get('/',(req,res)=>{
       day:'numeric' 
     }
     let headingdate = date.toLocaleDateString('en-us',options)
-    res.render('list',{headingdate:headingdate,todolist:tasks})
+    res.render('list',{listtype:headingdate,todolist:tasks})
+})
+app.get('/work',(req,res) => {
+    res.render('list',{listtype:'work',todolist:worktasks})
 })
 app.post('/',(req,res)=>{
   let task = req.body.task; 
-  tasks.push(task)   
-  res.redirect('/')
+  if(req.body.list === "work") {
+    worktasks.push(task)
+    res.redirect("/work")
+  }
+  else{
+    tasks.push(task)   
+    res.redirect('/')
+  }
 })
 app.listen(3000,()=>{
   console.log('server is running at port 3000')
