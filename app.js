b8ab2be9e767cc29c86c5fbd2b1fbb737fc316c2
LalitDhane <lalitdhane4455@gmail.com>
2021-06-14 21:13:39 +0530

    delete when checkbox is check

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ObjectId = require('mongodb').ObjectID
 const app = express()
 const date = require(__dirname+"/date.js")
 const Task = require(__dirname+"/models/task.js")
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
+
 mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
   console.log('DB connected')
   app.listen(3000,()=>{
@@ -14,18 +17,56 @@ mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser:true,us
 }).catch(err=>{
   console.error(err)
 })
+
+
 app.get('/',async (req,res)=>{
     let day = date.currdate()
-    const tasks = await Task.find({},{_id:0,content:1})
-    res.render('list',{listtype:day,todolist:tasks})
+    const tasks = await Task.find({},{content:1})
+    if(tasks.length === 0){
+      const item1 = new Task(
+        {
+          content:'Welcome to your todoList'
+        }
+      )
+      const item2 = new Task(
+        {
+          content:'to add a new item in todolist click the + icon down below'
+        }
+      )
+      const item3 = new Task(
+        {
+          content:'<---- click here to delete an item in the todolist'
+        }
+      )
+      await Task.insertMany([item1,item2,item3])
+      res.redirect('/')
+    }else{
+    res.render('list',{listtype:day,todolist:tasks})  
+    }
 })
+
+
 app.post('/',async (req,res)=>{
-  let task = new Task({
-    content:req.body.task
-  }); 
-  await task.save()
+  const checktask = req.body.task
+  if(checktask === '') {
+    console.log('task is null')
+  } else {
+    let task = new Task({
+      content:checktask
+    }); 
+    await task.save()
+  }
   res.redirect('/')
 })
+
+
+app.post('/delete',async (req,res)=>{
+  let itemcontent = req.body.checkbox 
+  await Task.deleteOne({content:itemcontent})
+  res.redirect('/') 
+})
+ 
+
 app.get("/about",(req,res)=>{
   res.render("about")
-})
+})   
