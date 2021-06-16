8ce034fb32bcc99643f59600b33f508a098ee25b
LalitDhane <lalitdhane4455@gmail.com>
2021-06-16 17:16:53 +0530

    connect to cloud db

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 const app = express()
 const List = require(__dirname+"/models/list.js").List
 const CustomL = require(__dirname+"/models/customlist.js")
+const url = 'mongodb+srv://lmd:lmdrdx123@todolist-cluster.nnsvl.mongodb.net/todolistDB?retryWrites=true&w=majority'
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
 
-mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
   console.log('DB connected')
   app.listen(3000,()=>{
   console.log('server is running on port 3000')
